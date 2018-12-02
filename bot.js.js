23e34565409097b7e581776af85b9297eaa81a52
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-02 21:53:56 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -79,17 +79,10 @@ client.unload = command => {
 };
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('Aleyküm selam,  hoş geldin ^^');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'Velactus) {
+  if (msg.content.toLowerCase() === 'velactus') {
     msg.reply('eu.velactus.club Pek Yakinda!');
   }
 });
-
 
 client.elevation = message => {
   if(!message.guild) {
